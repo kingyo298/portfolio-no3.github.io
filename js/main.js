1904264f28c6762fe43e1bb3319aed39b521382a
kingyo298 <71971716+kingyo298@users.noreply.github.com>
2021-03-14 16:29:05 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,3 +67,15 @@ $(function(){
     }
   });
 });
+
+'use strict';
+{
+  const setFillHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  window.addEventListener('resize', setFillHeight);
+
+  setFillHeight();
+}
